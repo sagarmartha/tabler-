cd5b46620dfb2b0a163ed64ab9c3674386598b07
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2023-11-17 16:27:11 +0530

    login page

---
+++ test/index.js
@@ -0,0 +1,12 @@
+function myfunction() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('passwordi').value;
+ 
+  if (email == 'sagar111@gmail.com' && password == '123456') {
+    alert("successfull")
+    window.location.href("index2.html")
+  }
+  else {
+    alert{"wrong entry invalid"}
+  }
+}
