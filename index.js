b7c997a7acad23b7b37ee03b5c1c9977747227bc
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2023-11-18 10:56:41 +0530

    new branch

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function myfunction() {
   var email = document.getElementById('email').value;
-  var password = document.getElementById('passwordi').value;
- 
+  var password = document.getElementById('password').value;
   if (email == 'sagar111@gmail.com' && password == '123456') {
     alert("successfull")
-    window.location.href("form.html")
+    window.open('/tabler website/index2.html');
   }
   else {
-    alert{"wrong entry invalid"}
+    alert("wrong entry invalid")
+    return false;
   }
 }
